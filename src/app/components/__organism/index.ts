23008b8a25c8017e537d3668d31445ddeeeb9ccc
Gagloshvili <KGagloshvili@oriflame.com>
2025-02-20 19:33:22 +0400

    added: sign up markup

---
+++ src/app/components/__organism/index.ts
@@ -0,0 +1,4 @@
+export { default as Form } from './form/Form'
+export { default as Header } from './header/Header'
+export { default as Sidebar } from './sideBar/SideBar'
+export { default as SignIpSection } from './signIpSection/SignIpSection'
