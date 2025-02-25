c0b9d973d8df001c636aed8a1e9171b61f6b7a71
Gagloshvili <KGagloshvili@oriflame.com>
2025-02-25 12:41:46 +0400

    added:link-create logic

--- src/app/components/__organism/linkItem/LinkItem.tsx
+++ src/app/components/__organism/linkItem/LinkItem.tsx
@@ -1,8 +1,68 @@
+// import Image from "next/image";
+// import Select from "../select/Select";
+// import { Input } from "../../__molecules";
+
+// const LinkItem = () => {
+//   return (
+//     <section className="w-full bg-[#FAFAFA] p-[20px] rounded-[12px] flex flex-col gap-3">
+//       <div className="w-full flex items-center justify-between">
+//         <div className="flex items-center gap-2">
+//           <div className="w-3 h-[6px] relative">
+//             <Image
+//               src={"/assets/2lines.svg"}
+//               alt={"linesLink"}
+//               fill
+//               sizes="(max-width: 768px) 100px, 135px"
+//             />
+//           </div>
+
+//           <p className="text-base font-bold leading-[24px] text-[#737373]">
+//             Link #1
+//           </p>
+//         </div>
+
+//         <button className="text-base font-normal leading-[24px] text-[#737373]">
+//           Remove
+//         </button>
+//       </div>
+//       <Select />
+//       <Input />
+//     </section>
+//   );
+// };
+
+// export default LinkItem;
+
 import Image from "next/image";
 import Select from "../select/Select";
 import { Input } from "../../__molecules";
+import { FieldErrors, UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { Dispatch, FC, SetStateAction } from "react";
+import { LinkItemType } from "../links/Links";
 
-const LinkItem = () => {
+export type LinkItemPropsType = {
+  register?: UseFormRegister<LinkItemType>;
+  errors?: FieldErrors<LinkItemType>;
+  setDropDown?: Dispatch<SetStateAction<boolean>>;
+  dropDown?: boolean;
+  setValue?: UseFormSetValue<LinkItemType>;
+
+  name?: string;
+  link?: string;
+  index?: number;
+};
+
+const LinkItem: FC<LinkItemPropsType> = ({
+  register,
+  errors,
+  setDropDown,
+  dropDown,
+  setValue,
+
+  name,
+  link,
+  index,
+}) => {
   return (
     <section className="w-full bg-[#FAFAFA] p-[20px] rounded-[12px] flex flex-col gap-3">
       <div className="w-full flex items-center justify-between">
@@ -17,16 +77,23 @@ const LinkItem = () => {
           </div>
 
           <p className="text-base font-bold leading-[24px] text-[#737373]">
-            Link #1
+            Link #{index}
           </p>
         </div>
 
-        <p className="text-base font-normal leading-[24px] text-[#737373]">
+        <button className="text-base font-normal leading-[24px] text-[#737373]">
           Remove
-        </p>
+        </button>
       </div>
-      <Select />
-      <Input />
+      <Select
+        register={register}
+        errors={errors}
+        setDropDown={setDropDown}
+        dropDown={dropDown}
+        setValue={setValue}
+        name={name}
+      />
+      <Input register={register} errors={errors} link={link} />
     </section>
   );
 };
--- src/app/components/__organism/select/Select.tsx
+++ src/app/components/__organism/select/Select.tsx
@@ -1,8 +1,153 @@
+// "use client";
+// import { platformData } from "@/app/commons/data";
+// import Image from "next/image";
+// import { FieldErrors, UseFormRegister } from "react-hook-form";
+// import { LinkItemType } from "../links/Links";
+// import { Dispatch, FC, SetStateAction } from "react";
+
+// export type SelectPropsType = {
+//   register: UseFormRegister<LinkItemType>;
+//   errors: FieldErrors<LinkItemType>;
+//   setDropDown: Dispatch<SetStateAction<boolean>>;
+//   dropDown: boolean;
+// };
+
+// const Select: FC<SelectPropsType> = ({
+//   register,
+//   errors,
+//   setDropDown,
+//   dropDown,
+// }) => {
+//   const getIconName = (platform: string): string => {
+//     const iconMap: { [key: string]: string } = {
+//       "Dev.to": "devto",
+//       "Frontend Mentor": "frontendmentor",
+//       "Stack Overflow": "stackoverflow",
+//     };
+//     return iconMap[platform] || platform.toLowerCase();
+//   };
+
+//   return (
+//     <div className="w-full">
+//       <label
+//         htmlFor="platform"
+//         className="text-[#333333] text-xs font-normal leading-[18px]"
+//       >
+//         Platform
+//       </label>
+
+//       <div className="w-full relative">
+//         <input
+//           {...register("name")}
+//           type="text"
+//           className="w-full text-[#333333] text-base font-normal leading-[18px] py-3 pl-4 rounded-[8px] outline-none "
+//           placeholder="Select Platform..."
+//         />
+
+//         <button className="px-4 py-2 flex items-center justify-center absolute right-0 top-0 z-10 transform translate-y-1/2">
+//           <div className="w-[12px] h-[6px] flex items-center justify-center relative">
+//             <Image
+//               src={"/assets/icons/icon-chevron-down.svg"}
+//               alt={"logo"}
+//               fill
+//             />
+//           </div>
+//         </button>
+
+//         {dropDown && (
+//           <div className="MAIN hidden w-full absolute left-0 right-0 z-10 top-[52px] bg-white  px-4 rounded-[8px] shadow-xl max-h-[200px] overflow-y-scroll">
+//             {Object.values(platformData).map((platform, i, arr) => {
+//               const isLastItem = i === arr.length - 1;
+//               const iconName = getIconName(platform);
+
+//               return (
+//                 <div
+//                   onClick={() => setDropDown(!dropDown)}
+//                   key={i}
+//                   className={`w-full flex items-center gap-3 py-3 ${
+//                     !isLastItem ? "border-b border-b-[#D9D9D9]" : ""
+//                   } cursor-pointer`}
+//                 >
+//                   <Image
+//                     src={`/assets/icons/select-icons/icon-${iconName}.svg`}
+//                     alt={`${platform} icon`}
+//                     width={16}
+//                     height={16}
+//                   />
+//                   <div className="w-full text-[#333333] text-base font-normal leading-[18px]">
+//                     {platform}
+//                   </div>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         )}
+
+//         {/* <div className="hidden w-full absolute left-0 right-0 z-10 top-[52px] bg-white  px-4 rounded-[8px] shadow-xl max-h-[200px] overflow-y-scroll">
+//           {Object.values(platformData).map((platform, i, arr) => {
+//             const isLastItem = i === arr.length - 1;
+//             const iconName = getIconName(platform);
+
+//             return (
+//               <div
+//               onClick={prev => setDropDown(!prev)}
+//                 key={i}
+//                 className={`w-full flex items-center gap-3 py-3 ${
+//                   !isLastItem ? "border-b border-b-[#D9D9D9]" : ""
+//                 } cursor-pointer`}
+//               >
+//                 <Image
+//                   src={`/assets/icons/select-icons/icon-${iconName}.svg`}
+//                   alt={`${platform} icon`}
+//                   width={16}
+//                   height={16}
+//                 />
+//                 <div className="w-full text-[#333333] text-base font-normal leading-[18px]">
+//                   {platform}
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </div> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Select;
+
 "use client";
 import { platformData } from "@/app/commons/data";
 import Image from "next/image";
+import { FieldErrors, UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { LinkItemType } from "../links/Links";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 
-const Select = () => {
+export type SelectPropsType = {
+  register?: UseFormRegister<LinkItemType>;
+  errors?: FieldErrors<LinkItemType>;
+  setDropDown?: Dispatch<SetStateAction<boolean>>;
+  dropDown?: boolean;
+  setValue?: UseFormSetValue<LinkItemType>;
+
+
+  name?: string;
+};
+
+const Select: FC<SelectPropsType> = ({
+  register,
+  errors,
+  setDropDown,
+  dropDown,
+  setValue,
+
+
+ name
+}) => {
+//   const [selectedPlatform, setSelectedPlatform] = useState<string>("");
+  const [selectedPlatform, setSelectedPlatform] = useState(name || "");
+  console.log(errors)
+
   const getIconName = (platform: string): string => {
     const iconMap: { [key: string]: string } = {
       "Dev.to": "devto",
@@ -10,6 +155,12 @@ const Select = () => {
       "Stack Overflow": "stackoverflow",
     };
     return iconMap[platform] || platform.toLowerCase();
+  };
+
+  const handleSelectPlatform = (platform: string) => {
+    setSelectedPlatform(platform);
+    setValue?.("name", platform); // Set value for "name"
+    setDropDown?.(false); // Close dropdown
   };
 
   return (
@@ -23,12 +174,21 @@ const Select = () => {
 
       <div className="w-full relative">
         <input
+          {...register?.("name")} // Registering the select input correctly
           type="text"
-          className="w-full text-[#333333] text-base font-normal leading-[18px] py-3 pl-4 rounded-[8px] outline-none "
+          onChange={(e) => setSelectedPlatform(e.target.value)}
+          value={selectedPlatform}
+          className="w-full text-[#333333] text-base font-normal leading-[18px] py-3 pl-4 rounded-[8px] outline-none"
           placeholder="Select Platform..."
+        //   defaultValue={name || ""}
         />
 
-        <button className="px-4 py-2 flex items-center justify-center absolute right-0 top-0 z-10 transform translate-y-1/2">
+        <button
+          onClick={() => setDropDown?.(!dropDown)}
+          type="button"
+        //   className="px-4 py-2 flex items-center justify-center absolute right-0 top-0 z-10 transform translate-y-1/2"
+          className={`${name && "hidden"} px-4 py-2 flex items-center justify-center absolute right-0 top-0 z-10 transform translate-y-1/2`}
+        >
           <div className="w-[12px] h-[6px] flex items-center justify-center relative">
             <Image
               src={"/assets/icons/icon-chevron-down.svg"}
@@ -38,13 +198,18 @@ const Select = () => {
           </div>
         </button>
 
-        <div className="hidden w-full absolute left-0 right-0 z-10 top-[52px] bg-white  px-4 rounded-[8px] shadow-xl max-h-[200px] overflow-y-scroll">
+        <div
+          className={`${
+            dropDown ? "block" : "hidden"
+          } w-full absolute left-0 right-0 z-10 top-[52px] bg-white px-4 rounded-[8px] shadow-xl max-h-[200px] overflow-y-scroll`}
+        >
           {Object.values(platformData).map((platform, i, arr) => {
             const isLastItem = i === arr.length - 1;
             const iconName = getIconName(platform);
 
             return (
               <div
+                onClick={() => handleSelectPlatform(platform)}
                 key={i}
                 className={`w-full flex items-center gap-3 py-3 ${
                   !isLastItem ? "border-b border-b-[#D9D9D9]" : ""
